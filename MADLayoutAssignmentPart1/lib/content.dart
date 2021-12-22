e8551d796472522b31d71303999ff3232c5f57ff
mrfaeez <faexd99@gmail.com>
2021-12-22 17:13:22 +0800

    add devices

---
+++ MADLayoutAssignmentPart1/lib/content.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text('Hello'),
+    ]);
+  }
+}
--- MADLayoutAssignmentPart1/lib/main.dart
+++ MADLayoutAssignmentPart1/lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import './content.dart';
 
 void main() => runApp(MyApp());
+
+class Devices {
+  late int id;
+  late String name;
+  late String icon;
+  bool state = false;
+
+  Devices({
+    required this.id,
+    required this.name,
+    required this.icon,
+    required this.state,
+  });
+}
 
 class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
+
+  final List<Devices> _devices = [
+    Devices(id: 0, name: 'Porch Light', icon: 'light-ceiling', state: false),
+    Devices(id: 1, name: 'Front Plug', icon: 'plug', state: false),
+    Devices(
+        id: 2,
+        name: 'Living Room Light',
+        icon: 'light-switch-on',
+        state: false),
+    Devices(id: 3, name: 'Living Room Fan', icon: 'fan', state: false),
+    Devices(id: 4, name: 'Kitchen Light', icon: 'lightbulb', state: false),
+    Devices(id: 5, name: 'Kitchen Downlight', icon: 'adjust', state: false),
+  ];
 }
 
 class _MyAppState extends State<MyApp> {
@@ -21,7 +49,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My Home'),
         ),
-        body: Center(),
+        body: Center(child: Column(children: <Widget>[Content()])),
       ),
     );
   }
